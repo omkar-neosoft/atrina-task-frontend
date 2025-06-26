e7f7979e6f13733c737eb26f5ecea6cc87d8f7eb
omkar-neosoft <omkar.mhadgut@neosoftmail.com>
2025-06-26 17:33:55 +0530

    added alert notification

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { OrderService } from './service/order/order.service';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  deliveryStatusUpdateAlert: string = '';
+
+  constructor(private orderService: OrderService) {}
+
+  ngOnInit() {
+    this.orderService.statusUpdate$.subscribe((update) => {
+      if (update) {
+        this.deliveryStatusUpdateAlert = `Delivery status for order id: ${update.orderId} is updated as: ${update.deliveryStatus}`;
+        console.log({
+          deliveryStatusUpdateAlert: this.deliveryStatusUpdateAlert,
+        });
+        setTimeout(() => {
+          this.deliveryStatusUpdateAlert = '';
+        }, 5000);
+      }
+    });
+  }
+}
